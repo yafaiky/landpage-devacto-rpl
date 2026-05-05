466cc5d5f8d79a2dcae056546162ea396c504d80
yafaiky <yafaiky@gmail.com>
2026-05-05 23:10:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GSAP from './components/gsap';
+import './App.css'
+
+function App() {
+  return (
+  <>
+    <h1 className="text-3xl font-bold underline">ini adalah test gsap!</h1>
+      <GSAP />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/gsap.jsx
@@ -0,0 +1,23 @@
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+// Register the hook as a plugin
+gsap.registerPlugin(useGSAP);
+
+const GSAP = () => {
+  const container = useRef();
+
+  // useGSAP handles cleanup automatically
+  useGSAP(() => {
+    gsap.to(".box", { rotation: 360, duration: 2 });
+  }, { scope: container });
+
+  return (
+    <div ref={container}>
+      <div className="box">Rotate Me!</div>
+    </div>
+  );
+};
+
+export default GSAP;
